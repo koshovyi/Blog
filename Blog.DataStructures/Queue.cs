ff8b938d83bb83670144cc2e15cfa81fbbb33a81
Dmytro Koshovyi <dima@grunger.net>
2021-03-03 00:06:15 +0200

    Queue, Stack

--- Blog.DataStructures/Queue.cs
+++ Blog.DataStructures/Queue.cs
@@ -1,8 +1,101 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Blog.DataStructures
 {
-	public class Queue
+
+	public class Queue<T> : IEnumerable<T>
 	{
+
+		private int _Front = -1;
+
+		private int _Rear = -1;
+
+		private int _Count = 0;
+
+		private readonly int _Size;
+
+		private readonly T[] _Array;
+
+		public Queue(int Size)
+		{
+			this._Size = Size;
+			this._Array = new T[Size];
+		}
+
+		public int Size
+		{
+			get { return _Size; }
+		}
+
+		public int Count
+		{
+			get { return _Count; }
+		}
+
+		public bool IsFull()
+		{
+			return _Rear == _Size - 1;
+		}
+
+		public bool IsEmpty()
+		{
+			return _Count == 0;
+		}
+
+		public void Enqueue(T Item)
+		{
+			if (this.IsFull())
+				throw new Exception("Очередь полностью заполнена.");
+
+			_Array[++_Rear] = Item;
+			_Count++;
+		}
+
+		public T Dequeue()
+		{
+			if (this.IsEmpty())
+				throw new Exception("Очередь не заполнена.");
+
+			T value = _Array[++_Front];
+			_Count--;
+			if (_Front == _Rear)
+			{
+				_Front = -1;
+				_Rear = -1;
+			}
+			return value;
+		}
+
+		public T Peek()
+		{
+			if (this.IsEmpty())
+				throw new Exception("Очередь не заполнена.");
+
+			T value = _Array[_Front + 1];
+			return value;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			if (this.IsEmpty())
+				throw new Exception("Очередь не заполнена.");
+
+			for (int i = _Front + 1; i <= _Rear; i++)
+				yield return _Array[i];
+		}
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+
 	}
+
 }
--- Blog.DataStructures/Stack.cs
+++ Blog.DataStructures/Stack.cs
@@ -1,8 +1,96 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Blog.DataStructures
 {
-	public class Stack
+	public class Stack<T> : IEnumerable<T>
 	{
+
+		private readonly int _Size;
+
+		private readonly T[] _Array;
+
+		private int _Top;
+
+		public Stack(int Size)
+		{
+			this._Size = Size;
+			this._Top = 0;
+			this._Array = new T[this._Size];
+		}
+
+		public int Top
+		{
+			get
+			{
+				return this._Top;
+			}
+		}
+
+		public int Size
+		{
+			get
+			{
+				return this._Size;
+			}
+		}
+
+		public int Count
+		{
+			get
+			{
+				return this._Top;
+			}
+		}
+
+		public bool IsFull()
+		{
+			return this._Top == this._Size;
+		}
+
+		public bool IsEmpty()
+		{
+			return this._Top == 0;
+		}
+
+		public void Push(T Element)
+		{
+			if (this.IsFull())
+				throw new Exception();
+
+			this._Array[this._Top++] = Element;
+		}
+
+		public T Peek()
+		{
+			return this._Array[this._Top - 1];
+		}
+
+		public T Pop()
+		{
+			return this._Array[--this._Top];
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			if (this.IsEmpty())
+				throw new Exception("Стек не заполнен.");
+
+			for (int i = this._Top; i > 0; i--)
+				yield return _Array[i - 1];
+		}
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+
 	}
+
 }
